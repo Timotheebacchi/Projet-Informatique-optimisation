5dea6e3f5a1cf9b59f370f30065f2609692eaa0a
Emeric Gandon <emericgandon81@gmail.com>
2024-07-11 16:04:46 +0200

    ele2

--- Site_flask/programme_remplissant_fichiertest.py
+++ Site_flask/programme_remplissant_fichiertest.py
@@ -6,7 +6,10 @@ listproj = ["glaciaire","jaugeages","montchauvel","MDE-réseau-qgis","democratie
         "germain-maureau","kleep","sursaut-gamma","engie-profiling","engie-actif","cbio-visu","keolis-offre","keolis-autonomie","scintigraphie3","datapred",
         "alumni-identifiant","puzzle","methane1","methane2","ocde-empreinte","chu-centrale-appel","pictet-trading-strategie","pictet-forecasting","vieillissement-mouche","scanner-low-cost","cristal"]
 n = len(listproj)
-listel = ["David Abulius","Valentin Allard","Amine Amzai","Gaspard Aractingi"]
+listel = ["David Abulius","Valentin Allard","Amine Amzai","Gaspard Aractingi","Nejma Araki","Baptiste Auscher","Madeleine Banessy",
+        "Mathis Bardot","Yannis Baron","Louis Barthelemy","Stephanie Bellini", "Gael Bigot", "Louis Bindel", "Denis Bonnand", "Fiona Bonnefoy Cudraz",
+        "Mattis Borderies", "Simon Boudara","Louis Boulay","Quentin Bouquet","Antoine BdlR","Charlotte Bourny","Julie Boyer","Esther Braconnier"
+        ]
 
 
 with open(fichier_csv, mode='w', newline='') as file:
